a91e168e6adca6a0333d05b73c3499310cb54f95
Ron Mesard <rmesard@digitas.com>
2017-06-20 22:01:57 -0400

    webform supporting JS code

--- sites/greyhoundamberalert.org/themes/gaa/js/script.js
+++ sites/greyhoundamberalert.org/themes/gaa/js/script.js
@@ -23,4 +23,32 @@
     }
   };
 
+	Drupal.behaviors.webform = {
+		attach: function(context, settings) {
+			$('#edit-submitted-missing-from').blur(function() {
+				console.log('edit-submitted-missing-from blurred, value:' + encodeURIComponent($(this).val()) + ', img: ' + $('.webform-component--missing-from-map img').attr('src'));
+				$('input[name="submitted[missing_from_hidden]"]').val(encodeURIComponent($(this).val()));
+				if (!!!$(this).val()) {
+					$('.webform-component--missing-from-map img').slideUp();
+				} else {
+					$('.webform-component--missing-from-map img').attr('src', '//maps.googleapis.com/maps/api/staticmap?center='+encodeURIComponent($(this).val())+'&markers='+encodeURIComponent($(this).val())+'&size=450x275&zoom=17&scale=2&key=AIzaSyAHkISHTA7qMB6Gl9BsFksuVF_huCYFJBI').slideDown();
+				}
+			});
+			$('#edit-submitted-dogs-name').blur(function() {
+				console.log('edit-submitted-dogs-name blurred, value:' + encodeURIComponent($(this).val()));
+				$('input[name="submitted[dogs_name_hidden]"]').val(encodeURIComponent($(this).val()));
+			});
+			$('#edit-submitted-contact-phone').blur(function() {
+				console.log('edit-submitted-contact-phone blurred, value:' + encodeURIComponent($(this).val()));
+				$('input[name="submitted[contact_phone_hidden]"]').val(encodeURIComponent($(this).val()));
+			});
+			$('#edit-submitted-contact-phone-2').blur(function() {
+				console.log('edit-submitted-contact-phone-2 blurred, value:' + encodeURIComponent($(this).val()));
+				$('input[name="submitted[contact_phone_2_hidden]"]').val(encodeURIComponent($(this).val()));
+			});
+		}
+	};
+
+
+
 })(jQuery, Drupal, this, this.document);
